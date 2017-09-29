1df3518c5288ae741ecae860fb0d55bac2476c3a
anirudhchellani <anirudhchellani@gmail.com>
2017-09-29 01:01:52 -0400

    Added database_name environment variable

--- environment_example.sh
+++ environment_example.sh
@@ -4,7 +4,7 @@ export datonate_env=[local/deploy]		#Determine whether in production or local
 export datonate_sql=[ip]							#MySQL server ip
 export datonate_sqlUser=[username]		#The username for the postgresql server
 export datonate_sqlPass=[password]		#The password for the postgresql user
-
+export datonate_sqldb=[dbname]
 
 
 
