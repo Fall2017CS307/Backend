cdc41bd14b93fe8af9f89b74d1cb23f12e2d5dd2
Anirudh <anirudhchellani@gmail.com>
2017-11-03 01:45:56 -0400

    Batch

--- fileHandler.py
+++ fileHandler.py
@@ -53,9 +53,75 @@ class fileHandler():
         fileArr.append(json.dumps(csvArr))
         print(fileArr[0])
         return fileArr
+
+    @staticmethod
+    def imgFile(zipF):
+        zipList = zipF.namelist()
+        print "imgFile"
+        files = []
+        fileNames = []
+        for fileName in zipList:
+            fileByte = str(zipF.read(fileName))
+            if imghdr.what(None,h=fileByte) is not None:
+                print fileName
+                fileArr = {}
+                fileNames.append(fileName)
+                fileArr['name'] = fileName
+                fileArr['fileByte'] = fileByte
+                files.append(fileArr)
+        if(len(files) <=0):
+            return None
+        dumpString = json.dumps(fileNames, ensure_ascii=False)
+        
+        fileArr = {}
+        fileArr['name'] = 'file_list.json'
+        fileArr['fileByte'] = dumpString
+        files.append(fileArr)
+        
+        return files
+
+    @staticmethod
+    def jsonFile(zipF):
+        zipList = zipF.namelist()
+        if(len(zipList)!=1):
+            return None
+        jsonArr = []
+        fileContent = str(zipF.read(zipList[0]))
+        try:
+            fileArr = json.loads(fileContent)
+            if(not (type(fileArr) is list)):
+                return None
+            for item in fileArr:
+                print item
+                if(not (type(item) is dict)):
+                    continue
+
+                question = str(item.get("question"))
+                print type(question)
+                if(not(type(question) is str)):
+                    continue
+                addInfo = str(item.get("addInfo"))
+
+                itemArr = {}
+                itemArr['question'] = question
+                itemArr['addInfo'] = addInfo
+                jsonArr.append(itemArr)
+
+            if(len(jsonArr) <1):
+                return None
+            
+        except:
+            print "Except"
+            return None
+        fileArr = {}
+        files = []
+        fileArr['name'] = 'file_list.json'
+        fileArr['fileByte'] = json.dumps(jsonArr)
+        files.append(fileArr)
+        return files
     
     @staticmethod
-    def uploadFile(user_id):
+    def uploadFile(user_id, datasetType):
 
         ret = {}
         user = userHandler().getUser(user_id)
@@ -69,6 +135,11 @@ class fileHandler():
             ret['errors'] = []
             ret['errors'].append("File not supplied")
             return apiDecorate(ret, 400, "File not supplied")
+        
+        if(datasetType!=0 and datasetType!=1):
+            ret['errors'] = []
+            ret['errors'].append("Invalid Type")
+            return apiDecorate(ret, 400, "Invalid Type")
 
         uploadedFile = request.files['file']
 
@@ -88,7 +159,7 @@ class fileHandler():
         randString = ''.join(random.choice('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ') for i in range(randint(10,20)))
         timeStamp = datetime.now()
         randName_noext = str(randNum) + str(timeStamp.year) + str(timeStamp.month) + randString + str(timeStamp.day) + str(timeStamp.hour) + str(timeStamp.minute) + str(timeStamp.second) + str(randNum2)
-        randName = randName_noext+".zip"
+        randName = randName_noext
         storageLocation = os.path.join(fileHandler.UPLOAD_FOLDER, randName)
         uploadedFile.save(storageLocation)
 
@@ -100,34 +171,25 @@ class fileHandler():
             return apiDecorate(ret, 400, "Invalid File")
         
         zipF = zipfile.ZipFile(storageLocation)
-        zipList = zipF.namelist()
-        countFiles = len(zipList)
-        #print countFiles
-        if(countFiles <= 0):
+        if(datasetType == 0):
+            fileArr = fileHandler.jsonFile(zipF)
+        else:
+            fileArr = fileHandler.imgFile(zipF)
+        if(fileArr is None):
             ret['errors'] = []
-            ret['errors'].append("Zip file empty")
+            ret['errors'].append("Zip file empty or contains invalid/incompatible files")
             return apiDecorate(ret, 400, "Zip file empty")
-        if(countFiles == 1):
-            fileArrr = fileHandler.csvFile(zipF,zipList)
-            if(fileArrr is None):
-                ret['errors'] = []
-                ret['errors'].append("Invalid File, File not a valid zip file")
-                return apiDecorate(ret, 400, "Invalid File")
-        fileRead = open(storageLocation, 'r+')
-
-
-
         botoConn = boto.connect_s3(fileHandler.DREAM_key, fileHandler.DREAM_secretKey, host="objects-us-west-1.dream.io")
-        bucket = botoConn.get_bucket(fileHandler.DREAM_Bucket, validate=True)
-        k = Key(bucket)
-        k.key = randName
-        sent = k.set_contents_from_file(fileRead, cb=None, md5=None, reduced_redundancy=False, rewind=True)
+        bucket = botoConn.get_bucket(fileHandler.DREAM_Bucket, validate=False)
 
+        for file in fileArr:
+            k = Key(bucket)
+            k.key = randName+"/"+file['name']
+            sent = k.set_contents_from_string(file['fileByte'], cb=None, md5=None, reduced_redundancy=False)
+            print len(file['fileByte'])
+            print sent
 
-        if sent != fileSize:
-            return "false"
-
-        data = models.dataset(user_id=user_id, file_name=uploadedFile.filename, resource_id=randName)
+        data = models.dataset(user_id=user_id, isMedia=1, resource_id=randName)
 
         session = dbConn().get_session(dbConn().get_engine())
         session.add(data)
--- route.py
+++ route.py
@@ -3,26 +3,30 @@ import MySQLdb
 from utils import dbConn
 from userHandler import userHandler
 from fileHandler import fileHandler
+from datasetHandler import datasetHandler
 app = Flask(__name__)
 
 app.add_url_rule('/api/login', 'api/login',userHandler().login, methods=['GET', 'POST'])
 app.add_url_rule('/api/register', 'api/register',userHandler().register, methods=['GET', 'POST'])
 app.add_url_rule('/api/verify/email/<string:key>', '/api/verify/email', userHandler().verify_email, methods=['GET', 'POST'])
 app.add_url_rule('/api/verify/phone/<string:key>', '/api/verify/phone', userHandler().verify_phone, methods=['GET', 'POST'])
-app.add_url_rule('/api/<int:user_id>/upload', '/api/user_id/upload', fileHandler.uploadFile, methods=['POST'])
+app.add_url_rule('/api/<int:user_id>/upload/<int:datasetType>', '/api/user_id/upload', fileHandler.uploadFile, methods=['POST'])
 app.add_url_rule('/api/<int:user_id>/datasets', '/api/user_id/datasets', userHandler().getDatasets, methods=['POST', 'GET'])
 
-app.add_url_rule('api/charge','api/charge', paymentHandler().charge, methods=['POST'])
+#app.add_url_rule('api/charge','api/charge', paymentHandler().charge, methods=['POST'])
 
 #^THIS MIGHT REQUIRE SOME MODIFICATION
 
-'/api/<int:user_id>/datasets', '/api/user_id/datasets',
+#'/api/<int:user_id>/datasets', '/api/user_id/datasets',
 
 app.add_url_rule('/api/<int:user_id>/dataset/delete/<int:dataset_id>/', '/api/user_id/dataset/delete/dataset_id', userHandler().deleteDataset, methods=['POST', 'GET'])
 app.add_url_rule('/api/<int:user_id>/dataset/public/<int:dataset_id>/', '/api/user_id/dataset/public/dataset_id', userHandler().makeDatasetPublic, methods=['POST', 'GET'])
 app.add_url_rule('/api/<int:user_id>/dataset/private/<int:dataset_id>/', '/api/user_id/dataset/private/dataset_id', userHandler().makeDatasetPrivate, methods=['POST', 'GET'])
 app.add_url_rule('/api/datasets/public', '/api/datasets/public', userHandler().getPublicDatasets, methods=['POST', 'GET'])
 app.add_url_rule('/api/<int:user_id>/dataset/copy/<int:dataset_id>/', '/api/user_id/dataset/copy/private/dataset_id', userHandler().copyPublicDataset, methods=['POST', 'GET'])
+
+#Create Experiments
+app.add_url_rule('/api/<int:user_id>/create/<int:dataset_id>/', '/api/user_id/create/dataset_id', datasetHandler.createExperiment, methods=['POST', 'GET'])
 
 
 if __name__ == '__main__':
