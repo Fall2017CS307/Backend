355c2384c00ce1536f4022a83207f6fcfe56c8d7
anirudhchellani <anirudhchellani@gmail.com>
2017-11-12 16:19:56 -0500

    User LOgin Test Case

--- tests/test.py
+++ tests/test.py
@@ -5,6 +5,10 @@ import MySQLdb
 import os
 import utils.dbConn as db
 import models
+import json
+import urllib2
+
+SERVER_ADDRESS = "http://127.0.0.1:5000"
 
 def test_db_config():
 	try:	
@@ -22,3 +26,25 @@ def test_table_creation():
 	except:
 		assert False, "Check previous test cases!"
 
+def test_login():
+	email = "achellan@purdue.edu"
+	password = "secret"
+	invalidEmail = "invalid@datonate.com"
+	invalidPassword="InvalidPassword"
+	
+	jsonString  = urllib2.urlopen(SERVER_ADDRESS+"/api/login?email="+email+"&password="+password).read()	
+	jsonArr = json.loads(jsonString)
+	if(jsonArr['status']!=200):
+		assert False,"Login Failed for valid user/password combination \n Response + " + str(jsonArr)
+	
+	jsonString  = urllib2.urlopen(SERVER_ADDRESS+"/api/login?email="+invalidEmail+"&password="+password).read()	
+	jsonArr = json.loads(jsonString)
+	if(jsonArr['status']==200):
+		assert False,"Login Success for invalid user \n Response + " + str(jsonArr)
+
+	jsonString  = urllib2.urlopen(SERVER_ADDRESS+"/api/login?email="+email+"&password="+invalidPassword).read()	
+	jsonArr = json.loads(jsonString)
+	if(jsonArr['status']==200):
+		assert False,"Login Success for invalid password \n Response + " + str(jsonArr)
+	
+	
